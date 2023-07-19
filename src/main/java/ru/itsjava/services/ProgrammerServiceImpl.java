e1b89a9e83ee210f451dcce7e7c0786e3021c119
Pavel Nacharkin <pavel.nacharkin@gmail.com>
2023-07-19 22:56:22 +0300

    Encoding

--- src/main/java/ru/itsjava/services/ProgrammerServiceImpl.java
+++ src/main/java/ru/itsjava/services/ProgrammerServiceImpl.java
@@ -1,20 +1,1 @@
-package ru.itsjava.services;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-@Service("programmerService")
-@RequiredArgsConstructor
-public class ProgrammerServiceImpl implements ProgrammerService {
-    private final NotebookService notebookService;
-    private final IOService ioService;
-
-    @Override
-    public void hiToNewProgrammer() {
-
-        System.out.println("Enter your name: ");
-        String name = ioService.input();
-        System.out.println("Hello " + name);
-        System.out.println("Your computer: " + notebookService.getNotebook());
-    }
-}
+package ru.itsjava.services;import lombok.RequiredArgsConstructor;import org.springframework.stereotype.Service;@Service("programmerService")@RequiredArgsConstructorpublic class ProgrammerServiceImpl implements ProgrammerService {    private final NotebookService notebookService;    private final IOService ioService;    @Override    public void hiToNewProgrammer() {        System.out.println("Enter your name: ");        String name = ioService.input();        System.out.println("Hello " + name);        System.out.println("Your computer: " + notebookService.getNotebook());    }}
